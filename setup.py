4f7c0d45493fc0b801b3e2f0002263e52272e6da
Jonathan Garson <garsonjonathan22@gmail.com>
2023-10-21 12:06:06 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project objective is to isolate keyword in some paragraphs in order to fasten the reading of docx or txt files',
+    author=': Jonathan Garson',
+    license='MIT',
+)
