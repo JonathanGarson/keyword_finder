55e7ec806189579491b1218e2504d649ba254fc1
JonathanGarson <jonathan.garson@sciencespo.fr>
2023-10-26 14:03:35 +0000

    à jamais les premiers

--- src/app/streamlit.py
+++ src/app/streamlit.py
@@ -30,33 +30,35 @@ def process_docx_files(docx_files, list_kw):
     merged_doc = Document()
 
     for docx_file in docx_files:
-        title = os.path.splitext(docx_file.name)[0]  # Extract the file name without extension
-        doc = Document(docx_file)
-        merged_doc.add_heading(title, level=1)  # Add the title as Header 1 to the merged document
+        with open(docx_file, "rb") as f: # Open the file in read-only mode
+            title = docx_file.split("/")[-1]  # Extract the file name without extension
+            print(title)
+            doc = Document(docx_file)
+            merged_doc.add_heading(title, level=1)  # Add the title as Header 1 to the merged document
 
-        for para in doc.paragraphs:
-            text = para.text
-            if len(re_highlight.findall(text)) > 0:
-                matches = re_highlight.finditer(text)
-                p3 = 0
-                highlighted_para = merged_doc.add_paragraph()  # Add a new paragraph to the merged document
-                for match in matches:
-                    p1 = p3
-                    p2, p3 = match.span()
-                    highlighted_para.add_run(text[p1:p2])
-                    run = highlighted_para.add_run(text[p2:p3])
-                    run.font.highlight_color = WD_COLOR.YELLOW
-                    highlighted_para.add_run(text[p3:])
+            for para in doc.paragraphs:
+                text = para.text
+                if len(re_highlight.findall(text)) > 0:
+                    matches = re_highlight.finditer(text)
+                    p3 = 0
+                    highlighted_para = merged_doc.add_paragraph()  # Add a new paragraph to the merged document
+                    for match in matches:
+                        p1 = p3
+                        p2, p3 = match.span()
+                        highlighted_para.add_run(text[p1:p2])
+                        run = highlighted_para.add_run(text[p2:p3])
+                        run.font.highlight_color = WD_COLOR.YELLOW
+                        highlighted_para.add_run(text[p3:])
 
     return merged_doc
 
-def merge_all_docx(docx_files, output_path):
-    merged_doc = Document()
-    for docx_file in docx_files:
-        doc = Document(docx_file)
-        for element in doc.element.body:
-            merged_doc.element.body.append(element)
-    merged_doc.save(output_path)
+# def merge_all_docx(docx_files, output_path):
+#     merged_doc = Document()
+#     for docx_file in docx_files:
+#         doc = Document(docx_file)
+#         for element in doc.element.body:
+#             merged_doc.element.body.append(element)
+#     merged_doc.save(output_path)
 
 def main():
     st.title("Keyword Finder App")
@@ -78,23 +80,37 @@ def main():
                 with open(temp_path, 'wb') as f:
                     f.write(docx_file.read())
                 docx_file_list.append(temp_path)
+            print(docx_file_list)
 
-             # Process the uploaded Word documents and get the merged DOCX document
+            # Process the uploaded Word documents and get the merged DOCX document
             merged_doc = process_docx_files(docx_file_list, list_kw)
+            with open(os.path.join(temp_dir, "final_merged.docx"), "wb") as f:
+                merged_doc.save(f)
 
-            merged_filename = st.text_input("Merged DOCX Filename", "final_merged")
+            # merged_filename = st.text_input("Merged DOCX Filename", "final_merged")
+                
+            # if st.button("Download Merged DOCX"):
+            #     st.markdown("### Download the merged DOCX file")
+            #     with st.spinner("Merging and saving..."):
+            #         # Merge the docx files and save the result
+            #         merge_all_docx(docx_file_list, os.path.join(temp_dir, merged_filename + ".docx"))
 
-            if st.button("Download Merged DOCX"):
-                st.markdown("### Download the merged DOCX file")
-                with st.spinner("Merging and saving..."):
-                    # Merge the docx files and save the result
-                    merge_all_docx(docx_file_list, os.path.join(temp_dir, merged_filename + ".docx"))
+            #         # Move the merged file to the user-specified location
+            output_path = os.path.join(temp_dir, "final_merged.docx")
+            #         shutil.move(output_path, ".")
 
-                    # Move the merged file to the user-specified location
-                    output_path = os.path.join(temp_dir, merged_filename + ".docx")
-                    shutil.move(output_path, ".")
+            #     st.success("Merged DOCX file has been saved!")
 
-                st.success("Merged DOCX file has been saved!")
+
+            with open(output_path, "rb") as file:
+                btn = st.download_button(
+                        label="Download docx",
+                        data=file,
+                        file_name="final_merged.docx",
+                        mime="application/octet-stream"
+                    )
+
+            st.success("Merged DOCX file has been saved!")
 
 if __name__ == "__main__":
     main()
